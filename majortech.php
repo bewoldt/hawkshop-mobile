558a44ed3553908421c11ec3ed0718ebadb0a5d9
Brandon Ewoldt <bewoldt1@gmail.com>
2016-05-27 15:56:57 -0500

    tech by major

---
+++ majortech.php
@@ -0,0 +1,199 @@
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Major Tech</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+	<link rel="stylesheet" href="css/p3.css">
+	<?php include 'includes/google-font.php'; ?>
+	<script src="js/modernizr.js"></script>
+	<script src="https://cdn.rawgit.com/scottjehl/picturefill/master/dist/picturefill.min.js"></script>
+</head>
+<body class="">
+	<a id="skiplink" href="#main-content">Skip to main content</a>
+
+	<?php include 'includes/header.php'; ?>
+
+	<main id="main-content" tabindex="-1" class="cd-main-content">
+		<?php include 'includes/subcats.php'; ?>
+		<div class="container">
+			<div class="column-main">
+				<div class="row">
+					<div class="col-md-12">
+						<?php include 'includes/breadcrumbs.php'; ?>
+						<div class="catalogcontent-heading">
+							<h1 class="catalog-title">Shop Technology by Major</h1>
+						</div>
+						<div class="catalogcontent">
+							<div class="row">
+								<div class="col-md-10 col-md-offset-1">
+									<h1 class="catalog-title">Major Tech - Our tech advice + your brains = Major Success</h1>
+									<h2>Get Tech Advice Specific to your major</h2>
+									<p>Stop by the Tech counter at the Iowa Hawk Shop and speak to our trained technology experts to learn what products will help you exceed in your major. Review computer recommendations and resources by your area of study. Our friendly staff is here to help with your computer purchase. Feel free to contact us with your questions at 319-335-3120 or <a href="mailto:techconnection@uiowa.edu">techconnection@uiowa.edu</a>.</p>
+
+									<h3>For Departments</h3>
+									<p>Are you a departmental representative with suggestions or comments about our recommendations? Would you like to add specific recommendations for students in your academic area? Please email us at <a href="mailto:techconnection@uiowa.edu">techconnection@uiowa.edu</a>.</p>
+									<br><br>
+									<div class="accordion">
+										<dl>
+											<dt>
+												<a href="#accordion1" aria-expanded="false" aria-controls="accordion1" class="accordion-title accordionTitle js-accordionTrigger">Business</a>
+											</dt>
+											<dd class="accordion-content accordionItem is-collapsed" id="accordion1" aria-hidden="true">
+												<div class="accordion-content-inner">
+													<table class="table table-bordered">
+														<tr>
+															<td>
+																<div class="accordion-product">
+																	<a href="#" class="accordion-img">
+																		<img src="http://www.hawkshop.com/webitemimages/103/W41284-t.jpg" alt="" >
+																	</a>
+																	<div class="accordion-info">
+																		<h5 class="accordion-title">
+																			<a href="#">MacBook Pro Retina 13 Inch 256GB</a>
+																		</h5>
+																		<p class="accordion-price">
+																			$1399.00
+																		</p>
+																	</div>
+																</div>
+															</td>
+															<td>
+																<div class="accordion-product">
+																	<a href="#">
+																		<img src="http://www.hawkshop.com/webitemimages/103/W41612-t.jpg" alt="" class="accordion-img">
+																	</a>
+																	<div class="accordion-info">
+																		<h5 class="accordion-title">
+																			<a href="#">MacBook Air 13-inch (128GB)</a>
+																		</h5>
+																		<p class="accordion-price">
+																			$949.00
+																		</p>
+																	</div>
+																</div>
+															</td>
+															<td>
+																<div class="accordion-product">
+																	<a href="#">
+																		<img src="http://www.hawkshop.com/webitemimages/103/W41602-t.jpg" alt="" class="accordion-img">
+																	</a>
+																	<div class="accordion-info">
+																		<h5 class="accordion-title">
+																			<a href="#">Dell Inspirion 500</a>
+																		</h5>
+																		<p class="accordion-price">
+																			$699.00
+																		</p>
+																	</div>
+																</div>
+															</td>
+														</tr>
+													</table>
+													<h3>Resources</h3>
+													<p>ITS Suggested Computer Configurations for Students<br>
+													<a href="http://its.uiowa.edu/support/article/100895">http://its.uiowa.edu/support/article/100895</a></p>
+												</div>
+											</dd>
+											<dt>
+												<a href="#accordion2" aria-expanded="false" aria-controls="accordion2" class="accordion-title accordionTitle js-accordionTrigger">
+												Second Accordion heading</a>
+											</dt>
+											<dd class="accordion-content accordionItem is-collapsed" id="accordion2" aria-hidden="true">
+												<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu interdum diam. Donec interdum porttitor risus non bibendum. Maecenas sollicitudin eros in quam imperdiet placerat. Cras justo purus, rhoncus nec lobortis ut, iaculis vel ipsum. Donec dignissim arcu nec elit faucibus condimentum. Donec facilisis consectetur enim sit amet varius. Pellentesque justo dui, sodales quis luctus a, iaculis eget mauris. </p>
+												<p>Aliquam dapibus, ante quis fringilla feugiat, mauris risus condimentum massa, at elementum libero quam ac ligula. Pellentesque at rhoncus dolor. Duis porttitor nibh ut lobortis aliquam. Nullam eu dolor venenatis mauris placerat tristique eget id dolor. Quisque blandit adipiscing erat vitae dapibus. Nulla aliquam magna nec elementum tincidunt.</p>
+											</dd>
+											<dt>
+												<a href="#accordion3" aria-expanded="false" aria-controls="accordion3" class="accordion-title accordionTitle js-accordionTrigger">
+												Third Accordion heading
+												</a>
+											</dt>
+											<dd class="accordion-content accordionItem is-collapsed" id="accordion3" aria-hidden="true">
+											 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu interdum diam. Donec interdum porttitor risus non bibendum. Maecenas sollicitudin eros in quam imperdiet placerat. Cras justo purus, rhoncus nec lobortis ut, iaculis vel ipsum. Donec dignissim arcu nec elit faucibus condimentum. Donec facilisis consectetur enim sit amet varius. Pellentesque justo dui, sodales quis luctus a, iaculis eget mauris. </p>
+											 <p>Aliquam dapibus, ante quis fringilla feugiat, mauris risus condimentum massa, at elementum libero quam ac ligula. Pellentesque at rhoncus dolor. Duis porttitor nibh ut lobortis aliquam. Nullam eu dolor venenatis mauris placerat tristique eget id dolor. Quisque blandit adipiscing erat vitae dapibus. Nulla aliquam magna nec elementum tincidunt.</p>
+										  </dd>
+										</dl>
+									 </div>
+
+
+								</div><!-- end .col -->
+							</div><!-- end .row -->
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<?php include 'includes/footer.php'; ?>
+		<div class="offscreen-exit"></div>
+	</main>
+
+	<?php include 'includes/mobilenav.php'; ?>
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="js/build/production.min.js"></script>
+	<script>
+		(function(){
+	var d = document,
+	accordionToggles = d.querySelectorAll('.js-accordionTrigger'),
+	setAria,
+	setAccordionAria,
+	switchAccordion,
+  touchSupported = ('ontouchstart' in window),
+  pointerSupported = ('pointerdown' in window);
+
+  skipClickDelay = function(e){
+	 e.preventDefault();
+	 e.target.click();
+  }
+
+		setAriaAttr = function(el, ariaType, newProperty){
+		el.setAttribute(ariaType, newProperty);
+	};
+	setAccordionAria = function(el1, el2, expanded){
+		switch(expanded) {
+		case "true":
+			setAriaAttr(el1, 'aria-expanded', 'true');
+			setAriaAttr(el2, 'aria-hidden', 'false');
+			break;
+		case "false":
+			setAriaAttr(el1, 'aria-expanded', 'false');
+			setAriaAttr(el2, 'aria-hidden', 'true');
+			break;
+		default:
+				break;
+		}
+	};
+//function
+switchAccordion = function(e) {
+  console.log("triggered");
+	e.preventDefault();
+	var thisAnswer = e.target.parentNode.nextElementSibling;
+	var thisQuestion = e.target;
+	if(thisAnswer.classList.contains('is-collapsed')) {
+		setAccordionAria(thisQuestion, thisAnswer, 'true');
+	} else {
+		setAccordionAria(thisQuestion, thisAnswer, 'false');
+	}
+	thisQuestion.classList.toggle('is-collapsed');
+	thisQuestion.classList.toggle('is-expanded');
+		thisAnswer.classList.toggle('is-collapsed');
+		thisAnswer.classList.toggle('is-expanded');
+
+	thisAnswer.classList.toggle('animateIn');
+	};
+	for (var i=0,len=accordionToggles.length; i<len; i++) {
+		if(touchSupported) {
+		accordionToggles[i].addEventListener('touchstart', skipClickDelay, false);
+	 }
+	 if(pointerSupported){
+		accordionToggles[i].addEventListener('pointerdown', skipClickDelay, false);
+	 }
+	 accordionToggles[i].addEventListener('click', switchAccordion, false);
+  }
+})();
+	</script>
+</body>
+</html>
